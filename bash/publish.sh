51f56f8eb46c0b99ae9dc7578f85f2291e3c4ccb
Cristi <cristi@draghici.net>
2019-01-06 17:04:13 +0100

    Automation

--- bash/publish.sh
+++ bash/publish.sh
@@ -43,21 +43,21 @@ WORK_DIR_PATH="$PROJECT/$WORK_DIR"
 PLUGIN_PATH="$PROJECT/$WORK_DIR/$PLUGIN"
 SVN_PATH="$PROJECT/$WORK_DIR/svn"
 
-rm -fR $WORK_DIR_PATH
-mkdir -p $PLUGIN_PATH
+rm -fR "$WORK_DIR_PATH"
+mkdir -p "$PLUGIN_PATH"
 
 # Go to the workdir and get the svn repo
 svn co -q "http://svn.wp-plugins.org/$PLUGIN" "$SVN_PATH" || default_error
 
 # Copy the plugin files
-cp -r $NEW_PLUGIN_SRC $PLUGIN_PATH
+cp -r "$NEW_PLUGIN_SRC" "$PLUGIN_PATH"
 
 # Move out the trunk directory to a temp location
-mv "$SVN_PATH/trunk" "WORK_DIR_PATH/svn-trunk"
+mv "$SVN_PATH/trunk" "$WORK_DIR_PATH/svn-trunk"
 
 # Make the necessary changes
 ## new trunk
-cp -r $PLUGIN_PATH "$SVN_PATH/trunk"
+cp -r "$PLUGIN_PATH" "$SVN_PATH/trunk"
 ## new assets
 mv "$SVN_PATH/trunk/assets" "$SVN_PATH/assets"
 ## new tag
@@ -65,7 +65,7 @@ mkdir -p "$SVN_PATH/trunk/tags/$VERSION"
 cp -r "$SVN_PATH/trunk/*" "$SVN_PATH/trunk/tags/$VERSION"
 
 # Copy all the .svn folders from the checked out copy of trunk to the new trunk
-cd "WORK_DIR_PATH/svn-trunk"
+cd "$WORK_DIR_PATH/svn-trunk"
 # Find all .svn dirs in sub dirs
 TARGET="$SVN_PATH/trunk"
 SVN_DIRS=`find . -type d -iname .svn`
